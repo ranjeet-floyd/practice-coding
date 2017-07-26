e1fe627272c97f601f2d31d724bc1d888ee21f7d
ranjeet kumar <ranjeet.kumar@spire2grow.com>
2017-07-26 23:10:55 +0530

    dp, tree and random

--- src/main/java/future/fry/practice/array/MaxProduct.java
+++ src/main/java/future/fry/practice/array/MaxProduct.java
@@ -14,16 +14,56 @@ public class MaxProduct {
         int[] arr = {10, 3, 5, 6, 20};
         //Output: 1200
         //Multiplication of 10, 6 and 20
+        
+        System.out.println(maxProductOfThree(arr, arr.length));
     }
 
     /*
     Find the max three number and product of these will be max
     | use k times quick sort
      */
-    private static int maxProductOfThree(int[] arr) {
+    private static int maxProductOfThree(int[] arr, int n) {
 
-//        int mid = 
-        return 0;
+        if (n < 3) {
+            return -1;
+        }
+
+        //max
+        int maxA = Integer.MIN_VALUE, maxB = Integer.MIN_VALUE, maxC = Integer.MIN_VALUE;
+        //min
+        int minA = Integer.MAX_VALUE, minB = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+
+            if (arr[i] > maxA) {
+                maxC = maxB;
+                maxB = maxA;
+                maxA = arr[i];
+            } else {
+
+                if (arr[i] > maxB) {
+                    maxC = maxB;
+                    maxB = arr[i];
+                } else {
+                    if (arr[i] > maxC) {
+                        maxC = arr[i];
+                    }
+
+                }
+            }
+            //update min
+            if (arr[i] < minA) {
+                minB = minA;
+                minA = arr[i];
+            } else {
+
+                if (arr[i] < minB) {
+                    minB = arr[i];
+                }
+            }
+
+        }
+        return Math.max(maxA * maxB * maxC, maxA * minA * minB);
 
     }
 }
